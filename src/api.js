4784cd6df2f0816234240f2370992c9d07d5d72c
Mason Fox <masonfox22@gmail.com>
2021-05-30 10:53:17 -0400

    resolve dotenv import

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-import * as ynab from 'ynab';
-const token = "970bf6d2668620e66174bd683a2504b54fbdc9ff2c52f0ebb4f37da1f3ab48d7";
+import * as ynab from "ynab";
+
+const token = process.env.VUE_APP_YNAB_TOKEN;
 
 const api = new ynab.API(token);
 
