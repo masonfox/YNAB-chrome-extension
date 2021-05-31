c9af843c13ff0bd17f493c7e9686273529f0fe97
Mason Fox <masonfox22@gmail.com>
2021-05-31 11:15:28 -0400

    formatting cleasn-up

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
-    <div id="app">
-        <Navbar />
-        <router-view class="mt-10" />
-    </div>
+  <div id="app">
+    <Navbar />
+    <router-view class="mt-10" />
+  </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
+import Navbar from "@/components/Navbar";
 
 export default {
-    name: 'App',
-    components: {
-        Navbar
-    }
-}
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
 <style>
 #app {
-    width: 300px;
-    height: 465px;
+  width: 300px;
+  height: 465px;
+}
+
+pre {
+  display: block;
+  font-family: Consolas;
+  white-space: pre-wrap;
+  background: #f5f5f5;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  border: 1px solid #ddd;
 }
 
 @tailwind base;
