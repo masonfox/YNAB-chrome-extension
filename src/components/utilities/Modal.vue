3e1b543a58d64edd19056a547fad6302e8b177a8
Mason Fox <masonfox22@gmail.com>
2021-06-08 00:00:12 -0400

    spice-up reset modal

--- src/components/utilities/Modal.vue
+++ src/components/utilities/Modal.vue
@@ -3,7 +3,7 @@
     class="absolute inset-0 z-30 overflow-hidden bg-black bg-opacity-30 h-full w-full"
   >
     <section class="inset-y-0 h-full flex justify-center items-center">
-      <div class="w-3/4">
+      <div class="w-10/12">
         <div class="h-full flex flex-col p-5 bg-white shadow-xl rounded">
           <slot></slot>
         </div>
--- src/views/AddTransactionView.vue
+++ src/views/AddTransactionView.vue
@@ -211,6 +211,25 @@
     <!-- Regular Modals -->
     <Modal v-show="visibleModals.ClearForm">
       <div>
+        <div
+          class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 mb-2.5"
+        >
+          <svg
+            class="h-6 w-6 text-red-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+        </div>
         <div class="mb-6 text-center">
           <h3 class="font-bold text-base mb-1">Are you sure?</h3>
           <p class="text-sm text-gray-400">
@@ -219,13 +238,13 @@
         </div>
         <div class="flex justify-around">
           <button
-            class="text-center px-3 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-0"
+            class="text-center py-1.5 w-20 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-0"
             @click="toggleSelectModalVisibility('ClearForm')"
           >
             Cancel
           </button>
           <button
-            class="text-center px-3 py-1 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-0"
+            class="text-center py-1.5 w-20 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-0"
             @click="[toggleSelectModalVisibility('ClearForm'), resetForm()]"
           >
             Clear
